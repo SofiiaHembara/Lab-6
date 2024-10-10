b83e94c827fdc88d84208671a722e1b8ba8e648c
Sofiia Hembara <hembara.pn@ucu.edu.ua>
2024-10-10 17:21:59 +0300

    Add FlowerBucket and related classes

---
+++ src/main/java/flower/store/Store.java
@@ -0,0 +1,18 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private List<FlowerBucket> flowerBuckets = new ArrayList<>();
+
+    public void addFlowerBucket(FlowerBucket bucket) {
+        flowerBuckets.add(bucket);
+    }
+
+    public List<FlowerBucket> search (Flower flower) {
+        return flowerBuckets.stream()
+            .filter(bucket -> bucket.getPrice() > 0)
+            .toList();
+    }
+}
