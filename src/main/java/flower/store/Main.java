22ff4484604b894f3f0f27b854fbaa065e65230c
Sofiia Hembara <hembara.pn@ucu.edu.ua>
2024-10-12 23:42:07 +0300

    delete unnessery files

--- src/main/java/flower/store/Main.java
+++ src/main/java/flower/store/Main.java
@@ -5,19 +5,5 @@ public class Main {
         Flower flower = new Flower();
         flower.setPrice(100);
         flower.setFlowerType(FlowerType.ROSE);
-
-        FlowerBucket flowerBucket = new FlowerBucket();
-
-        FlowerPack flowerPack = new FlowerPack(flower, 5);
-        flowerBucket.addFlowerPack(flowerPack);
-
-        FlowerPack flowerPack2 = new FlowerPack(flower, 10); 
-        flowerBucket.addFlowerPack(flowerPack2);
-
-        System.out.println(flowerBucket.getPrice());
-
-        flower.setPrice(10);
-
-        System.out.println(flowerBucket.getPrice());
     }
 }
